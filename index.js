d466958c0ae926fe40d1e6c4938e0b6acd266d85
Leon <leonwheeler08@gmail.com>
2021-05-21 23:22:07 +0100

    Added initial list question with read and exit.

--- index.js
+++ index.js
@@ -5,9 +5,39 @@ const database = new Db("employees_db");
 
 const init = async () => {
   await database.start();
-  const employees = await database.allData("employee");
-  console.table(employees);
-  database.end();
+
+  let inProgress = true;
+
+  while (inProgress) {
+    const selectOption = {
+      type: "list",
+      message: "Choose an option from the list below:",
+      name: "option",
+      choices: [
+        {
+          name: "--- View all employees ---",
+          value: "VIEWEMPLOYEES",
+        },
+        {
+          name: "--- Exit app ---",
+          value: "EXIT",
+        },
+      ],
+    };
+    const { choice } = await inquirer.prompt(selectOption);
+
+    if (choice === "VIEWEMPLOYEES") {
+      await console.log("view");
+      const employees = await database.allData("employee");
+      await console.table(employees);
+    }
+
+    if (choice === "EXIT") {
+      console.log("exit");
+      inProgress = false;
+      database.end();
+    }
+  }
 };
 
 init();
