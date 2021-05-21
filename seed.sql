4e8d866064c9a95877ee7c7d408c693962667817
Leon <leonwheeler08@gmail.com>
2021-05-21 07:06:54 +0100

    Fixed foreign key format and changed order of tables being created.

--- seed.sql
+++ seed.sql
@@ -2,22 +2,26 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE employee (
+
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  lat_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL FOREIGN KEY,
-  manager_id INT FOREIGN KEY
+  name VARCHAR(30)
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT FOREIGN KEY
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
-CREATE TABLE department (
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30)
-)
+  first_name VARCHAR(30) NOT NULL,
+  lat_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
