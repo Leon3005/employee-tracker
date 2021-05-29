87e92e438d9c723b7010a52d1cf65e88a326f9bb
Leon <leonwheeler08@gmail.com>
2021-05-29 16:23:57 +0100

    Created updateData file and moved update manager and role functions.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const database = new Db("employees_db");
 
 //Importing add, update, and delete functions
 const { addEmployee, addRole, addDepartment } = require("./src/utils/addData");
+const { updateManager, updateRole } = require("./src/utils/updateData");
 
 //Creating the function to run all of the questions/app.
 const init = async () => {
@@ -105,78 +106,11 @@ const init = async () => {
     }
 
     if (choice === "UPDATEMANAGER") {
-      //Makes all employees available to be chosen.
-      const employees = await database.selectAll("employee");
-
-      const generateChoicesEmployee = (emps) => {
-        return emps.map((emp) => {
-          return {
-            short: emp.id,
-            name: emp.first_name + " " + emp.last_name,
-            value: emp.id,
-          };
-        });
-      };
-      const updateManagerQ = [
-        {
-          type: "list",
-          message: "Choose the employee you would like to update:",
-          name: "id",
-          choices: generateChoicesEmployee(employees),
-        },
-        {
-          type: "list",
-          message: "Choose the manager you would like to add:",
-          name: "manager_id",
-          choices: generateChoicesEmployee(employees),
-        },
-      ];
-
-      const { manager_id, id } = await inquirer.prompt(updateManagerQ);
-      //Updates the employees manager ID
-      await database.updateOne("employee", { manager_id }, "id", id);
+      await updateManager();
     }
 
     if (choice === "UPDATEROLE") {
-      const employees = await database.selectAll("employee");
-      const allRoles = await database.selectAll("role");
-
-      const generateChoicesEmployee = (emps) => {
-        return emps.map((emp) => {
-          return {
-            short: emp.id,
-            name: emp.first_name + " " + emp.last_name,
-            value: emp.id,
-          };
-        });
-      };
-      const generateChoicesRole = (roles) => {
-        return roles.map((role) => {
-          return {
-            short: role.id,
-            name: role.title,
-            value: role.id,
-          };
-        });
-      };
-      const updateRoleQ = [
-        {
-          type: "list",
-          message: "Choose the employee you would like to update:",
-          name: "id",
-          choices: generateChoicesEmployee(employees),
-        },
-        {
-          type: "list",
-          message: "Choose the role you would like to change to:",
-          name: "role_id",
-          choices: generateChoicesRole(allRoles),
-        },
-      ];
-
-      //Updates the employees role ID
-      const { role_id, id } = await inquirer.prompt(updateRoleQ);
-      await database.updateOne("employee", { role_id }, "id", id);
+      await updateRole();
     }
 
     if (choice === "DELETEEMPLOYEE") {
---
+++ src/utils/updateData.js
@@ -0,0 +1,81 @@
+const inquirer = require("inquirer");
+
+const Db = require("../db/database");
+const database = new Db("employees_db");
+
+const updateManager = async () => {
+  //Makes all employees available to be chosen.
+  const employees = await database.selectAll("employee");
+
+  const generateChoicesEmployee = (emps) => {
+    return emps.map((emp) => {
+      return {
+        short: emp.id,
+        name: emp.first_name + " " + emp.last_name,
+        value: emp.id,
+      };
+    });
+  };
+  const updateManagerQ = [
+    {
+      type: "list",
+      message: "Choose the employee you would like to update:",
+      name: "id",
+      choices: generateChoicesEmployee(employees),
+    },
+    {
+      type: "list",
+      message: "Choose the manager you would like to add:",
+      name: "manager_id",
+      choices: generateChoicesEmployee(employees),
+    },
+  ];
+
+  const { manager_id, id } = await inquirer.prompt(updateManagerQ);
+  //Updates the employees manager ID
+  await database.updateOne("employee", { manager_id }, "id", id);
+};
+
+const updateRole = async () => {
+  const employees = await database.selectAll("employee");
+  const allRoles = await database.selectAll("role");
+
+  const generateChoicesEmployee = (emps) => {
+    return emps.map((emp) => {
+      return {
+        short: emp.id,
+        name: emp.first_name + " " + emp.last_name,
+        value: emp.id,
+      };
+    });
+  };
+  const generateChoicesRole = (roles) => {
+    return roles.map((role) => {
+      return {
+        short: role.id,
+        name: role.title,
+        value: role.id,
+      };
+    });
+  };
+  const updateRoleQ = [
+    {
+      type: "list",
+      message: "Choose the employee you would like to update:",
+      name: "id",
+      choices: generateChoicesEmployee(employees),
+    },
+    {
+      type: "list",
+      message: "Choose the role you would like to change to:",
+      name: "role_id",
+      choices: generateChoicesRole(allRoles),
+    },
+  ];
+
+  //Updates the employees role ID
+  const { role_id, id } = await inquirer.prompt(updateRoleQ);
+  await database.updateOne("employee", { role_id }, "id", id);
+};
+
+module.exports = { updateManager, updateRole };
